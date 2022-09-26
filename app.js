34577e9ada104750883457be0003e0ac5c1547fa
VanshArora08 <kumar.vansharora@gmail.com>
2022-09-26 15:28:45 +0530

    updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 const ejs = require("ejs");
 const _ = require('lodash');
 const mongoose=require('mongoose');
@@ -12,8 +13,8 @@ const dailySchema=new mongoose.Schema({
   data:String
 })
 
-const AdminPassword="Insane@4101441014";
-const viewPassword="YouAreTheBest";
+const AdminPassword=process.env.ADMIN_PASSWORD;
+const viewPassword=process.env.VIEW_PASSWORD;
 const Day=mongoose.model("Day",dailySchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
